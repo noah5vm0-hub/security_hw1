768aa0724a82bb7febeb2bea6e8c6206a5cc3bda
Huang, Jhang-Syuan <noah5vm0@gmail.com>
2026-05-06 19:25:55 +0800

    Add SQL Injection test form in securityhw1.php

--- securityhw1.php
+++ securityhw1.php
@@ -30,4 +30,42 @@ CRIPT_NAME"]);
 	}
 }
 ?>
-
+<html>
+<head>
+<title>SQL Injection Test 1</title>
+</head>
+<body>
+<div style="text-align: center;"><span>
+<big style="font-weight: bold; color: red;"><big><big>SQL Injection 練習</big></
+big></big><br>
+<br>
+請輸入登入的帳號與密碼</span><br>
+</div>
+<br>
+<form method="post" name="form1" id="form1">
+<table align="center">
+ <tbody>
+ <tr>
+   <td align="left" valign="top">
+     <div> &nbsp;&nbsp;UserID&nbsp; <input name="username" id="username" type="t
+ext"> </div>
+   </td>
+   <td align="left" valign="top">
+     <div> &nbsp;&nbsp;Password&nbsp; <input name="password" id="password" type=
+"password"> </div>
+   </td>
+   <td align="center" valign="middle"> <input name="login" id="login" value="Log
+in" type="submit"> </td>
+  </tr>
+ </tbody>
+</table>
+</form>
+<div style="text-align: center;"><br>
+<span style="font-weight: bold; color: rgb(0, 102, 0);">請勿隨意嘗試 SQL Injecti
+on 攻擊 </span>
+<br style="font-weight: bold; color: rgb(0, 102, 0);">
+<span style="font-weight: bold; color: rgb(0, 102, 0);">密碼錯誤三次將會暫停使用
+</span>
+</div>
+</body>
+</html>
